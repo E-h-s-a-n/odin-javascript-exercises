e783845fbcb656d95dd0ecaf0596ef88edf921af
Ehsan <ehsan440@rocketmail.com>
2022-09-07 21:38:10 +0430

    Complete exercis 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(text = '') {
+    let reversed = '';
+    let splitted = text.split('');
+    // console.log('text=', text)
+    for(let i=0; i<text.length; i++){
+        reversed += splitted.pop();
+    }
+    return reversed;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+console.log(reverseString('test!'));
