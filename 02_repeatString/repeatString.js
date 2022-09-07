d053cbecd51c9422ad80fe56d4656c6c061f7f19
Ehsan <ehsan440@rocketmail.com>
2022-09-07 20:58:34 +0430

    Complete exercis 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(text, repeatNum) {
+    result = ''
+    if (repeatNum < 0) result='ERROR'
+    
+    for(let i=0; i<repeatNum; i++){
+        result += text
+    }
 
+    return result
 };
 
 // Do not edit below this line
