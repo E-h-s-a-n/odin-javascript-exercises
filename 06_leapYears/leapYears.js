a0f2dd951d08a7fb52c0fccbcc523babb41ee514
Ehsan <ehsan440@rocketmail.com>
2022-09-08 20:03:38 +0430

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = false;
+    if ((year % 4) === 0 && (year % 25) !== 0) {
+        result = true
+    }
+    if ((year % 16) === 0) {
+        result = true
+    }
+    
+    return result
 };
 
 // Do not edit below this line
