3529354aec625d9cf39ac15892442c9252134e68
Ehsan <ehsan440@rocketmail.com>
2022-09-09 01:32:32 +0430

    Modify exercise 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,23 +1,17 @@
 const removeFromArray = function(inputArray, ...items) {
-    for (const i of items){
-        idx = inputArray.indexOf(i);
-        if (idx !== -1){
-            inputArray.splice(idx, 1);
-        };
-    };
-    return inputArray
+    // for (const i of items){
+    //     idx = inputArray.indexOf(i);
+    //     if (idx !== -1){
+    //         inputArray.splice(idx, 1);
+    //     };
+    // };
+    const result = inputArray.filter(element => {
+        return !items.includes(element);
+    });
+    return result;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
 
 // console.log(removeFromArray([1, 2, 3, 4, 5], 'a', 2, 'ba'))
-
-// let newArray = ['me', , , , , 'i']
-// //Shows all indexes, including deleted
-// newArray.find( (value, index) => {
-//     // delete elements on first iteration
-//     if(index === 0) {newArray.length = 0}
-//     console.log(`index: ${index}, value: ${value}`)
-// });
-// console.log(newArray)
