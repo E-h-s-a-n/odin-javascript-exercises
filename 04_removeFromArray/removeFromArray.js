0f809c8193989bdf6400a0fdd658150c0fd55ba9
Ehsan <ehsan440@rocketmail.com>
2022-09-08 17:16:06 +0430

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...items) {
+    for (const i of items){
+        idx = inputArray.indexOf(i);
+        if (idx !== -1){
+            inputArray.splice(idx, 1);
+        };
+    };
+    return inputArray
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// console.log(removeFromArray([1, 2, 3, 4, 5], 'a', 2, 'ba'))
+
+// let newArray = ['me', , , , , 'i']
+// //Shows all indexes, including deleted
+// newArray.find( (value, index) => {
+//     // delete elements on first iteration
+//     if(index === 0) {newArray.length = 0}
+//     console.log(`index: ${index}, value: ${value}`)
+// });
+// console.log(newArray)
