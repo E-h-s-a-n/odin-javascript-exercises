87a1a2480526b7a53d7858695bf121b43034c343
Ehsan <ehsan440@rocketmail.com>
2022-09-08 17:49:07 +0430

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    result = 0;
+    for (let i=start; i <= stop; i++){
+        result += i;
+    };
 
+    return result
 };
+
+console.log(sumAll(1, 5))
 
 // Do not edit below this line
 module.exports = sumAll;
+
