90218b53d509f8c9b100a57c635591db2112dab9
Ehsan <ehsan440@rocketmail.com>
2022-09-09 00:31:45 +0430

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius = (temp - 32) * 5 / 9;
+  return round(celsius, 1);
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let fahrenheit = temp * 9 / 5 + 32
+  return round(fahrenheit, 1)
+};
 
+function round(number, precision = 0) {
+  const i = Math.pow(10, precision);
+  return Math.round(number * i) / i;
 };
 
 // Do not edit below this line
