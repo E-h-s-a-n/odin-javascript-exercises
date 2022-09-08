d12e30edf6e0b8c25aaf341b4ee2d860aba23357
Ehsan <ehsan440@rocketmail.com>
2022-09-08 18:23:51 +0430

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,26 @@
 const sumAll = function(start, stop) {
-    result = 0;
-    for (let i=start; i <= stop; i++){
-        result += i;
+    let result = 0;
+    let min = start;
+    let max = stop;
+
+    if (start<0 || stop<0) { 
+        result = 'ERROR'
+    } else if (typeof start !== 'number' || typeof stop !== 'number'){
+        result = 'ERROR';
+    } else if (start > stop) {
+        min = stop;
+        max = start;
+    }
+    if (result === 0) {
+        for (let i=min; i <= max; i++){
+            result += i;
+        };
     };
 
-    return result
+    return result;
 };
-
-console.log(sumAll(1, 5))
 
 // Do not edit below this line
 module.exports = sumAll;
 
+// console.log(sumAll(1, 5))
