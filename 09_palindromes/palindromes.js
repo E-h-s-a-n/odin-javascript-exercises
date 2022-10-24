9e1e08178fba1267b6d435070284ffc27123a094
ehsan <13008704+E-h-s-a-n@users.noreply.github.com>
2022-10-25 00:53:28 +0330

    Add solution to 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    let result
+    words = words.toLowerCase()
 
+    const aa = [...words].filter((v) => {
+        return 'abcdefghijklmnopqrstuvwxyz'.includes(v)
+    });
+
+    return (aa.join("") === aa.reverse().join(""))
+
+    // const len = aa.length
+    // for (let i = 0; i < len/2; i++) {
+    //     result = aa[i] == aa[len-i-1]
+    //     if (!result) break
+    // }
+
+
+    return result
 };
+
+palindromes('Racecar!')
 
 // Do not edit below this line
 module.exports = palindromes;
